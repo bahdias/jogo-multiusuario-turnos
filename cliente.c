dd0d60001e94ce6b10f425fc1c754484df433e50
Paulo Victor <p.victorsousapro@gmail.com>
2023-10-11 15:26:42 -0300

    cliente x servidor

--- cliente.c
+++ cliente.c
@@ -1,136 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
-// Definição de elementos e suas vantagens
-#define FOGO 1
-#define AGUA 2
-#define TERRA 3
-#define AR 4
-
-int ataque_jogador(int elemento_jogador[], int posicao1, int posicao2, int defendeu) {
-    //Danos que serão realizados ao oponente com base no tipo de ataque do jogador
-    if ((elemento_jogador[posicao1] == FOGO && elemento_jogador[posicao2] == TERRA) ||
-        (elemento_jogador[posicao1] == AGUA && elemento_jogador[posicao2] == FOGO) ||
-        (elemento_jogador[posicao1] == TERRA && elemento_jogador[posicao2] == AR) ||
-        (elemento_jogador[posicao1] == AR && elemento_jogador[posicao2] == AGUA)) {
-        // Ataque Forte
-        if (defendeu == 1){
-            printf("Jogador %d realizou um ataque forte! -2 pontos pro Jogador %d que se defendeu do ataque\n", posicao1 + 1, posicao2 + 1);
-            return 2; // Retorna 2 para indicar ataque forte, mas com defesa do oponente
-        }else{
-            printf("Jogador %d realizou um ataque forte! -3 pontos pro Jogador %d que nao se defendeu do ataque\n", posicao1 + 1, posicao2 + 1);
-            return 3; // Retorna 3 para indicar ataque forte, sem defesa do oponente
-        }
-        
-    } else if ((elemento_jogador[posicao1] == FOGO && elemento_jogador[posicao2] == AGUA) ||
-               (elemento_jogador[posicao1] == AGUA && elemento_jogador[posicao2] == TERRA) ||
-               (elemento_jogador[posicao1] == TERRA && elemento_jogador[posicao2] == AR) ||
-               (elemento_jogador[posicao1] == AR && elemento_jogador[posicao2] == FOGO)) {
-        // Ataque Regular
-        if (defendeu == 1){
-            printf("Jogador %d realizou um ataque regular. -1 pontos pro Jogador %d que se defendeu do ataque\n", posicao1 + 1, posicao2 + 1);
-            return 1; // Retorna 1 para indicar ataque regular, mas com defesa do oponente
-        }else{
-            printf("Jogador %d realizou um ataque regular. -2 pontos pro Jogador %d que nao se defendeu do ataque\n", posicao1 + 1, posicao2 + 1);
-            return 2; // Retorna 2 para indicar ataque regular, sem defesa do oponente
-        }
-    } else if ((elemento_jogador[posicao1] == FOGO && elemento_jogador[posicao2] == AR) ||
-              (elemento_jogador[posicao1] == AGUA && elemento_jogador[posicao2] == TERRA) ||
-              (elemento_jogador[posicao1] == TERRA && elemento_jogador[posicao2] == FOGO) ||
-              (elemento_jogador[posicao1] == AR && elemento_jogador[posicao2] == AGUA)) {
-        // Ataque Fraco (quando nenhum dos casos acima se aplica)
-        if (defendeu == 1){
-            printf("Jogador %d realizou um ataque fraco. -0 pontos pro Jogador %d que se defendeu do ataque\n", posicao1 + 1, posicao2 + 1);
-            return 0; // Retorna 0 para indicar ataque fraco, mas com defesa do oponente
-        }else{
-            printf("Jogador %d realizou um ataque fraco. -1 ponto pro Jogador %d que nao se defendeu do ataque\n", posicao1 + 1, posicao2 + 1);
-            return 1; // Retorna 1 para indicar ataque fraco, sem defesa do oponente
-        }
-    } else{
-        printf("Jogador %d realizou um ataque nulo\n", posicao1 + 1);
-        return 0; //Ataque nulo
-    }
-}
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main() {
-    int pv_jogador[2] = {20, 20}; //Todos os jogadores começam com 20 pontos de vida
-    int elemento_jogador[2] = {0, 0}; //Lista para guardar o elemento escolhido pelos jogadores
-    int acao_jogador[2] = {0,0}; //Ação que os jogadores irão escolher (Ataque/Defesa)
+    int clientSocket;
+    struct sockaddr_in serverAddr;
+    int pv; // Pontos de vida do jogador
 
-    srand(time(NULL)); // Inicializa o gerador de números aleatórios
-    printf("Bem-vindo a Batalha dos Elementos!\n");
+    clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(1234);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-    while (pv_jogador[0] > 0 && pv_jogador[1] > 0) {
-        // Embaralhe os elementos aleatoriamente para ambos os jogadores
-        int elementos_disponiveis[4] = {FOGO, AGUA, TERRA, AR}; //Os 4 elementos disponíveis
-        int defendeu[2] = {0,0}; // false (não defende)
-        for (int i = 0; i < 2; i++) {
-            //for para gerar o elemento aleatório para cada jogador
-            int indice_aleatorio = rand() % 4;
-            elemento_jogador[i] = elementos_disponiveis[indice_aleatorio];
-            // Remove o elemento escolhido do vetor
-            for (int j = indice_aleatorio; j < 3; j++) {
-                elementos_disponiveis[j] = elementos_disponiveis[j + 1];
-            }
-        }
-
-        // Turno do Jogador 1
-        printf("Elemento Jogador 1: ");
-        if (elemento_jogador[0] == FOGO) {
-            printf("FOGO\n");
-        } else if (elemento_jogador[0] == AGUA) {
-            printf("AGUA\n");
-        } else if (elemento_jogador[0] == TERRA) {
-            printf("TERRA\n");
-        } else if (elemento_jogador[0] == AR) {
-            printf("AR\n");
-        }
-        printf("Jogador 1, escolha uma acao:\n");
-        printf("1 - Atacar\n");
-        printf("2 - Defender\n");
-        scanf("%d", &acao_jogador[0]);
-
-        // Turno do Jogador 2
-        srand(time(NULL)); // Reinicializa o gerador de números aleatório
-        if (elemento_jogador[1] == FOGO){
-            printf("Elemento: FOGO\n", elemento_jogador[1]);
-        }else if (elemento_jogador[1] == AGUA){
-            printf("Elemento: AGUA\n", elemento_jogador[1]);
-        }else if (elemento_jogador[1] == TERRA){
-            printf("Elemento: TERRA\n", elemento_jogador[1]);
-        }else if (elemento_jogador[1] == AR){
-            printf("Elemento: AR\n", elemento_jogador[1]);
-        }
-        printf("Jogador 2, escolha uma acao:\n");
-        printf("1 - Atacar\n");
-        printf("2 - Defender\n");
-        scanf("%d", &acao_jogador[1]);
-
-        // Lógica de combate
-        if (acao_jogador[0] == 2) {
-            defendeu[0] = 1;
-            printf("Jogador 1 defendeu.\n");
-        }if (acao_jogador[1] == 1) {
-            pv_jogador[0] -= ataque_jogador(elemento_jogador, 1, 0, defendeu[0]); //Penaliza o Jogador dependendo do ataque realizado
-        }else{
-            defendeu[1] = 1;
-            printf("Jogador 2 defendeu.\n");
-        }
-        if (acao_jogador[0] == 1) { 
-            pv_jogador[1] -= ataque_jogador(elemento_jogador, 0, 1, defendeu[1]); //Ataque sem defesa
-        }
-
-        // Exibir pontos de vida
-        printf("Pontos de Vida - Jogador 1: %d, Jogador 2: %d\n", pv_jogador[0], pv_jogador[1]);
+    if (connect(clientSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0) {
+        perror("Connect falhou");
+        exit(1);
     }
 
-    // Determinar o vencedor
-    if (pv_jogador[0] <= 0) {
-        printf("Jogador 2 venceu!\n");
-    } else {
-        printf("Jogador 1 venceu!\n");
+    while (1) {
+    int elemento, acao, pv;
+
+    recv(clientSocket, &pv, sizeof(pv), 0);
+    if (pv <= 0) {
+        printf("Você perdeu!\n");
+        break;
     }
-    
-    return 0;
+
+    printf("Seus pontos de vida: %d\n", pv);
+
+    printf("Escolha um elemento (1- Água, 2- Terra, 3- Fogo, 4- Ar): ");
+    scanf("%d", &elemento);
+    send(clientSocket, &elemento, sizeof(elemento), 0);
+
+    printf("Escolha uma ação (1- Atacar, 2- Defender): ");
+    scanf("%d", &acao);
+    send(clientSocket, &acao, sizeof(acao), 0);
+
+    // [Receba o resultado do servidor e imprima para o jogador]
+
+    printf("Esperando a escolha do oponente...\n");
+    // [Receba as escolhas do oponente e imprima para o jogador]
+}
+
+close(clientSocket);
+
+return 0;
 }
--- servidor.c
+++ servidor.c
@@ -1,8 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+// Função para calcular o resultado do combate
+int combateElementos(int elementoJogador, int elementoOponente) {
+    if (elementoJogador == elementoOponente) return 0; // Empate
+    if ((elementoJogador == 1 && elementoOponente == 4) ||
+        (elementoJogador == 2 && elementoOponente == 1) ||
+        (elementoJogador == 3 && elementoOponente == 2) ||
+        (elementoJogador == 4 && elementoOponente == 3)) {
+        return -1; // Jogador ganha
+    }
+    return 1; // Oponente ganha
+}
 
 int main() {
-    
+    int serverSocket, clientSocket1, clientSocket2, addrSize;
+    struct sockaddr_in serverAddr, clientAddr;
+    int pv1 = 100, pv2 = 100; // Pontos de vida dos jogadores
 
-    return 0;
+    serverSocket = socket(PF_INET, SOCK_STREAM, 0);
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(1234);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+
+    if (bind(serverSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0) {
+        perror("Bind falhou");
+        exit(1);
+    }
+
+    if (listen(serverSocket, 5) < 0) {
+        perror("Listen falhou");
+        exit(1);
+    }
+
+    printf("Aguardando conexão do Jogador 1...\n");
+    addrSize = sizeof(clientAddr);
+    clientSocket1 = accept(serverSocket, (struct sockaddr *) &clientAddr, &addrSize);
+    printf("Jogador 1 conectado.\n");
+
+    printf("Aguardando conexão do Jogador 2...\n");
+    clientSocket2 = accept(serverSocket, (struct sockaddr *) &clientAddr, &addrSize);
+    printf("Jogador 2 conectado.\n");
+
+    while (1) {
+    int elemento1, elemento2, acao1, acao2, resultado;
+
+    send(clientSocket1, &pv1, sizeof(pv1), 0);
+    send(clientSocket2, &pv2, sizeof(pv2), 0);
+
+    if (pv1 <= 0) {
+        printf("Jogador 1 perdeu!\n");
+        break;
+    }
+    if (pv2 <= 0) {
+        printf("Jogador 2 perdeu!\n");
+        break;
+    }
+
+    // Recebendo escolhas do Jogador 1
+    recv(clientSocket1, &elemento1, sizeof(elemento1), 0);
+    printf("Jogador 1 escolheu o elemento: %d\n", elemento1);
+    recv(clientSocket1, &acao1, sizeof(acao1), 0);
+    printf("Jogador 1 escolheu a ação: %d\n", acao1);
+
+    // Recebendo escolhas do Jogador 2
+    recv(clientSocket2, &elemento2, sizeof(elemento2), 0);
+    printf("Jogador 2 escolheu o elemento: %d\n", elemento2);
+    recv(clientSocket2, &acao2, sizeof(acao2), 0);
+    printf("Jogador 2 escolheu a ação: %d\n", acao2);
+
+    resultado = combateElementos(elemento1, elemento2);
+
+    if (acao1 == 1 && acao2 == 2) resultado = 0;
+    if (acao1 == 2 && acao2 == 1) resultado = -1;
+
+    if (resultado == 1) {
+        pv2 -= 10; // Jogador 2 perde PV
+        printf("Jogador 1 ganha esta rodada. PV do Jogador 2: %d\n", pv2);
+    } else if (resultado == -1) {
+        pv1 -= 10; // Jogador 1 perde PV
+        printf("Jogador 2 ganha esta rodada. PV do Jogador 1: %d\n", pv1);
+    } else {
+        printf("Empate nesta rodada. PVs mantidos.\n");
+    }
+}
+
+close(clientSocket1);
+close(clientSocket2);
+close(serverSocket);
+
+return 0;
 }
